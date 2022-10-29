babf97af15f82c5a13429133c2c908a952c70f05
Marian-Catalin Vaduva <marian.catalin1994@yahoo.com>
2022-10-29 20:59:43 +0300

    Python library implementation for 3relind-rpi
    Signed-off-by: Marian-Catalin Vaduva <marian.catalin1994@yahoo.com>

---
+++ python/3relind/README.md
@@ -0,0 +1,142 @@
+# lib3relind
+
+This is the python library to control the [Three 40A/240V Relays RS485 Daisy-chainable HAT for Raspberry Pi](https://sequentmicrosystems.com/collections/all-io-cards/products/three-40a-240v-relays-rs485-for-raspberry-pi).
+
+## Install
+
+```bash
+~$ sudo apt-get update
+~$ sudo apt-get install build-essential python-pip python-dev python-setuptools git
+~$ sudo pip install -U minimalmodbus
+~$ sudo pip install -U smbus
+~$ git clone https://github.com/SequentMicrosystems/3relind-rpi.git
+~$ cd 3relind-rpi/python/3relind/
+~/3relind-rpi/python/3relind$ sudo python setup.py install
+```
+For python3.x:
+```bash
+~$ sudo apt-get update
+~$ sudo apt-get install build-essential python3-pip python3-dev python3-smbus git
+~$ sudo pip3 install -U minimalmodbus
+~$ git clone https://github.com/SequentMicrosystems/3relind-rpi.git
+~$ cd 3relind-rpi/python/3relind/
+~/3relind-rpi/python/3relind$ sudo python3 setup.py install
+```
+## Update
+
+```bash
+~$ cd 3relind-rpi/
+~/3relind-rpi$ git pull
+~$ cd 3relind-rpi/python/3relind/
+~/3relind-rpi/python/3relind$ sudo python setup.py install
+```
+For python3.x the last line must be replaced with:
+```bash
+~/3relind-rpi/python/3relind$ sudo python3 setup.py install
+```
+## Usage 
+
+Now you can import the lib3relind library and use its functions. To test, read relays status from the board with address 0:
+
+```bash
+~$ python
+Python 2.7.9 (default, Sep 17 2016, 20:26:04)
+[GCC 4.9.2] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import lib3relind
+>>> lib3relind.get_all(0)
+0
+>>>
+```
+
+## Functions
+
+### set(address, relay, value)
+Set one relay state.
+
+address - address of the 3-Relay card (selectable from address microswitches [0..7])
+
+relay - relay number (id) [1..3]
+
+value - relay state 1: turn ON, 0: turn OFF[0..1]
+
+
+### set_all(address, value)
+Set all relays state.
+
+address - address of the 3-Relay card (selectable from address microswitches [0..7])
+
+value - 3 bit value of all relays (ex: 255: turn on all relays, 0: turn off all relays, 1:turn on relay #1 and off the rest)
+
+### get(address, relay)
+Get one relay state.
+
+address - address of the 3-Relay card (selectable from address microswitches [0..7])
+
+relay - relay number (id) [1..3]
+
+return 0 == relay off; 1 - relay on
+
+### get_all(address)
+Return the state of all relays.
+
+address - address of the 3-Relay card (selectable from address microswitches [0..7])
+
+return - [0..7]
+
+### mset(boardAddress, relay, value, baudrate = 38400, parity = 0, stopbits = 1)
+Set one modbus relay state.
+
+boardAddress - address of the 3-Relay card (selectable from address microswitches [1..254])
+
+relay - relay number (id) [1..3]
+
+value - relay state 1: turn ON, 0: turn OFF[0..1]
+
+baudrate - baud rate parameter for RS485 interface (default: 38400)
+
+parity - parity parameter for RS485 interface (default: 0 - None)
+
+stopbits - stop bits parameter for RS485 interface (default: 1)
+
+
+### mset_all(boardAddress, value, baudrate = 38400, parity = 0, stopbits = 1)
+Set all modbus relays state.
+
+boardAddress - address of the 3-Relay card (selectable from address microswitches [1..254])
+
+value - 3 bit value of all relays (ex: 255: turn on all relays, 0: turn off all relays, 1:turn on relay #1 and off the rest)
+
+baudrate - baud rate parameter for RS485 interface (default: 38400)
+
+parity - parity parameter for RS485 interface (default: 0 - None)
+
+stopbits - stop bits parameter for RS485 interface (default: 1)
+
+### mget(boardAddress, relay, baudrate = 38400, parity = 0, stopbits = 1)
+Get one modbus relay state.
+
+boardAddress - address of the 3-Relay card (selectable from address microswitches [1..254])
+
+relay - relay number (id) [1..3]
+
+baudrate - baud rate parameter for RS485 interface (default: 38400)
+
+parity - parity parameter for RS485 interface (default: 0 - None)
+
+stopbits - stop bits parameter for RS485 interface (default: 1)
+
+return 0 == relay off; 1 - relay on
+
+### mget_all(boardAddress, baudrate = 38400, parity = 0, stopbits = 1)
+Return the state of all modbus relays.
+
+boardAddress - address of the 3-Relay card (selectable from address microswitches [1..254])
+
+baudrate - baud rate parameter for RS485 interface (default: 38400)
+
+parity - parity parameter for RS485 interface (default: 0 - None)
+
+stopbits - stop bits parameter for RS485 interface (default: 1)
+
+return - [0..7]
---
+++ python/3relind/lib3relind/__init__.py
@@ -0,0 +1,210 @@
+import smbus
+import serial
+import minimalmodbus
+
+# bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+
+DEVICE_ADDRESS = 0x60  # 7 bit address (will be left shifted to add the read write bit)
+
+RELAY3_INPORT_REG_ADD = 0x00
+RELAY3_OUTPORT_REG_ADD = 0x01
+RELAY3_POLINV_REG_ADD = 0x02
+RELAY3_CFG_REG_ADD = 0x03
+
+
+def __check(bus, add):
+    cfg = bus.read_byte_data(add, RELAY3_CFG_REG_ADD)
+    if cfg != 0:
+        bus.write_byte_data(add, RELAY3_CFG_REG_ADD, 0)
+        bus.write_byte_data(add, RELAY3_OUTPORT_REG_ADD, 0)
+    return bus.read_byte_data(add, RELAY3_INPORT_REG_ADD)
+
+
+def DecToBin(value):
+    binary = [1 if digit=='1' else 0 for digit in bin(value)[2:]]
+    binary.reverse()
+    zero = [0, 0, 0]
+    if len(binary) < 3:
+        zero[:len(binary)] = binary
+        binary = zero
+    elif len(binary) > 3:
+        binary = binary[0:3]
+    return binary
+
+
+def BinToDec(binary):
+    return sum(val*(2**idx) for idx, val in enumerate(binary))
+
+
+def __setup_modbus(boardAddress, baudrate, parity, stopbits):
+    if boardAddress < 1 or boardAddress > 254:
+        raise ValueError('Invalid modbus board address!')
+    if baudrate < 1200 or baudrate > 920600:
+        raise ValueError('Invalid modbus baudrate!')
+    if parity < 0 or parity > 2:
+        raise ValueError('Invalid modbus parity!')
+    if stopbits < 1 or stopbits > 2:
+        raise ValueError('Invalid modbus stop bits!')
+    board = minimalmodbus.Instrument('/dev/serial0', boardAddress, debug = False)
+    board.serial.baudrate = baudrate
+    board.serial.bytesize = 8
+    board.serial.stopbits = stopbits
+    if parity == 0:
+        board.serial.parity = serial.PARITY_NONE
+    if parity == 1:
+        board.serial.parity = serial.PARITY_EVEN
+    if parity == 2:
+        board.serial.parity = serial.PARITY_ODD
+    board.mode = minimalmodbus.MODE_RTU
+    board.clear_buffers_before_each_transaction = True
+    return board
+
+
+def __check_modbus_board(board, relay = 0):
+    try:
+        value = board.read_bit(relay, functioncode = 1)
+    except Exception as e:
+        board.close_port_after_each_call = True
+        raise ValueError('3-relay card not detected!')
+    return value
+
+
+def set(address, relay, value):
+    if address < 0 or address > 7:
+        raise ValueError('Invalid board address!')
+    if relay < 1:
+        raise ValueError('Invalid relay number!')
+    if relay > 3:
+        raise ValueError('Invalid relay number!')
+    bus = smbus.SMBus(1)
+    hwAdd = DEVICE_ADDRESS + address
+    try:
+        oldVal = __check(bus, hwAdd)
+    except Exception as e:
+        bus.close()
+        raise ValueError('3-relay card not detected!')
+    try:
+        if value == 0:
+            oldVal = oldVal & (~(1 << (relay - 1)))
+            bus.write_byte_data(hwAdd, RELAY3_INPORT_REG_ADD, oldVal)
+        else:
+            oldVal = oldVal | (1 << (relay - 1))
+            bus.write_byte_data(hwAdd, RELAY3_INPORT_REG_ADD, oldVal)
+    except Exception as e:
+        bus.close()
+        raise ValueError('Fail to write relay state value!')
+    bus.close()
+
+
+def set_all(address, value):
+    if address < 0 or address > 7:
+        raise ValueError('Invalid board address!')
+    if value > 255:
+        raise ValueError('Invalid relay value!')
+    if value < 0:
+        raise ValueError('Invalid relay value!')
+    bus = smbus.SMBus(1)
+    hwAdd = DEVICE_ADDRESS + address
+    try:
+        oldVal = __check(bus, hwAdd)
+    except Exception as e:
+        bus.close()
+        raise ValueError('3-relay card not detected!')
+    try:
+        bus.write_byte_data(hwAdd, RELAY3_INPORT_REG_ADD, value)
+    except Exception as e:
+        bus.close()
+        raise ValueError('Fail to write relay state value!')
+    bus.close()
+
+
+def get(address, relay):
+    if address < 0 or address > 7:
+        raise ValueError('Invalid board address!')
+    if relay < 1:
+        raise ValueError('Invalid relay number!')
+    if relay > 3:
+        raise ValueError('Invalid relay number!')
+    bus = smbus.SMBus(1)
+    hwAdd = DEVICE_ADDRESS + address
+    try:
+        val = __check(bus, hwAdd)
+    except Exception as e:
+        bus.close()
+        raise ValueError('3-relay card not detected!')
+    val = val & (1 << (relay - 1))
+    bus.close()
+    if val == 0:
+        return 0
+    else:
+        return 1
+
+
+def get_all(address):
+    if address < 0 or address > 7:
+        raise ValueError('Invalid board address!')
+    bus = smbus.SMBus(1)
+    hwAdd = DEVICE_ADDRESS + address
+    try:
+        val = __check(bus, hwAdd)
+    except Exception as e:
+        bus.close()
+        raise ValueError('3-relay card not detected!')
+    bus.close()
+    return val
+
+
+def mset(boardAddress, relay, value, baudrate = 38400, parity = 0, stopbits = 1):
+    if relay < 1 or relay > 3:
+        raise ValueError('Invalid relay number!')
+    if value < 0 or value > 1:
+        raise ValueError('Invalid relay value!')
+    relay = relay - 1
+    board = __setup_modbus(boardAddress, baudrate, parity, stopbits)
+    __check_modbus_board(board)
+    try:
+        board.write_bit(relay, value, functioncode = 5)
+    except Exception as e:
+        board.close_port_after_each_call = True
+        raise ValueError('Fail to write relay state value!')
+    board.close_port_after_each_call = True
+
+
+def mset_all(boardAddress, value, baudrate = 38400, parity = 0, stopbits = 1):
+    if value < 0 or value > 255:
+        raise ValueError('Invalid relay value!')
+    board = __setup_modbus(boardAddress, baudrate, parity, stopbits)
+    __check_modbus_board(board)
+    valueBin = DecToBin(value)
+    try:
+        board.write_bits(0, valueBin)
+    except Exception as e:
+        board.close_port_after_each_call = True
+        raise ValueError('Fail to write relay state value!')
+    board.close_port_after_each_call = True
+
+
+def mget(boardAddress, relay, baudrate = 38400, parity = 0, stopbits = 1):
+    if relay < 1 or relay > 3:
+        raise ValueError('Invalid relay number!')
+    relay = relay - 1
+    board = __setup_modbus(boardAddress, baudrate, parity, stopbits)
+    value = __check_modbus_board(board, relay)
+    board.close_port_after_each_call = True
+    if value == 0:
+        return 0
+    else:
+        return 1
+
+
+def mget_all(boardAddress, baudrate = 38400, parity = 0, stopbits = 1):
+    board = __setup_modbus(boardAddress, baudrate, parity, stopbits)
+    __check_modbus_board(board)
+    try:
+        valueBin = board.read_bits(0, 3, functioncode = 1)
+    except Exception as e:
+        board.close_port_after_each_call = True
+        raise ValueError('Fail to read relay state values!')
+    board.close_port_after_each_call = True
+    value = BinToDec(valueBin)
+    return value
